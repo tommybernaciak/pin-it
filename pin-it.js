92062ab5f39a9962c691478131a6915b290fc6c0
tommybernaciak <tommybernaciak@gmail.com>
2017-03-27 12:37:35 +0100

    allow to apply anytime

--- pin-it.js
+++ pin-it.js
@@ -1,70 +1,74 @@
 (function ($) {
   "use strict";
   $.fn.pin = function (options) {
-    var scrollY = 0, elements = [], disabled = false
+    var scrollY  = 0,
+        elements = [];
     options = options || {};
     var $window = options.windowSelector ? $(options.windowSelector) : $(window);
+    var initScroll = $window.scrollTop();
 
-    var recalculateLimits = function () {
+    function recalculateLimits() {
       for (var i=0, len=elements.length; i<len; i++) {
         var $this = elements[i];
-
         var $container = options.containerSelector ? $this.closest(options.containerSelector) : $(document.body);
-        var pinOffset = $this.offset();
-        var containerOffset = $container.offset();
-        var windowOffset = $this.offsetParent().offset();
+        var pinOffset = $this.offset().top;
+        var containerOffset = $container.offset().top;
+        var windowOffset = $window.offset().top;
 
         if (!$this.parent().is(".pin-wrapper")) {
           $this.wrap("<div class='pin-wrapper'>");
         }
 
         $this.data("pin", {
-          from: options.containerSelector ? containerOffset.top : pinOffset.top,
-          to: containerOffset.top + $container.height() - $this.outerHeight(),
-          end: containerOffset.top + $container.height(),
-          windowTop: windowOffset.top
+          from: options.containerSelector ? containerOffset : pinOffset,
+          to: containerOffset + $container.height() - $this.outerHeight(),
+          end: containerOffset + $container.height(),
+          windowTop: windowOffset
         });
 
         $this.css({width: $this.outerWidth()});
         $this.parent().css("height", $this.outerHeight());
       }
-    };
+    }
 
-    var onScroll = function () {
-      // if (disabled) { return; }
-
-      scrollY = $window.scrollTop();
-      // var windowOffset = $window.offset();
+    function onScroll() {
+      scrollY = $window.scrollTop() - initScroll;
 
       for (var i=0, len=elements.length; i<len; i++) {
         var $this = $(elements[i]),
-            data  = $this.data("pin"),
-            from  = data.from,
-            to    = data.to,
-            windowTop = data.windowTop;
+            data  = $this.data("pin");
+        if (!data) { continue; }
+        var from      = data.from,
+            to        = data.to,
+            windowTop = data.windowTop,
+            end       = data.end;
 
-        if (from + $this.outerHeight() > data.end) {
+        if (from + $this.outerHeight() > end) {
           $this.css('position', '');
           continue;
         }
 
         if (from - windowTop < scrollY && to - windowTop > scrollY) {
           !($this.css("position") == "fixed") && $this.css({
-            left: $this.offset().left,
-            top: windowTop
+            top: windowTop,
+            left: $this.offset().left
           }).css("position", "fixed");
         } else if (scrollY >= to - windowTop) {
           $this.css({
-            left: "auto",
-            top: to - windowTop
+            top: to - windowTop + initScroll,
+            left: "auto"
           }).css("position", "absolute");
         } else {
-          $this.css({position: "", top: "", left: ""});
+          $this.css({
+            position: "",
+            top: "",
+            left: ""
+          });
         }
       }
-    };
+    }
 
-    var update = function () { recalculateLimits(); onScroll(); };
+    function update() { recalculateLimits(); onScroll(); }
 
     this.each(function () {
       var $this = $(this),
@@ -80,7 +84,6 @@
     $window.scroll(onScroll);
     $window.resize(function () { recalculateLimits(); });
     recalculateLimits();
-
     $window.on('load', update);
 
     return this;
